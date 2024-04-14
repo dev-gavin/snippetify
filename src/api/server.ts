8bd1e9f188c74b8a0c2c33bafac701bdf36ddba7
Gavin Meeker <gmeeker99@gmail.com>
2024-04-14 16:17:49 -0400

    feat: use prisma instead of custom db functionality

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,29 +1,38 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import { createSnippet, getSnippet } from "../db";
+import { PrismaClient } from "@prisma/client";
+import { CreateSnippetReq } from "../types/api/api.types";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(morgan("dev")); // use morgan middleware globally
 app.use(cors());
 app.use(express.json()); // allows client to send json
+
 app.use(express.urlencoded({ extended: true })); // de/encodes url properly to help handle query strings
 // urlencoded turns params of url into object on the req.params prop
 
-// TODO: need to the snippet ID more clear, it actually passing an object
 app.get("/snippets/:snippetId", async (req, res) => {
   const snippetId = req.params;
-  const snippetData = await getSnippet(snippetId);
-
-  res.json(snippetData);
-});
-
-app.post("/snippets", async (req, res) => {
-  const snippetData = req.body;
-  console.log(await createSnippet(snippetData));
 
   res.send();
+});
+
+app.post("/snippets", async (req: CreateSnippetReq, res) => {
+  const { title, content, userId } = req.body;
+
+  const createdSnippet = await prisma.snippet.create({
+    data: {
+      title: title,
+      content: content,
+      created_by: Number(userId),
+    },
+  });
+
+  res.send(createdSnippet);
 });
 
 export default app;
