4456c62cb74f735ca88be286335b29c50c40aa8d
Gavin Meeker <gmeeker99@gmail.com>
2024-04-13 23:04:11 -0400

    chore: add cors package

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { createSnippet, getSnippet } from "../db";
 
 const app = express();
 
 app.use(morgan("dev")); // use morgan middleware globally
+app.use(cors());
 app.use(express.json()); // allows client to send json
 app.use(express.urlencoded({ extended: true })); // de/encodes url properly to help handle query strings
 // urlencoded turns params of url into object on the req.params prop
