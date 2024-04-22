090dc3f7dd4b818f11d72cb53ae0c11134718505
Gavin Meeker <gmeeker99@gmail.com>
2024-04-22 06:35:41 -0400

    feat: useDebounce Hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+// value is state that can be changed
+export function useDebounce<T>(value: T, delay = 1000) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      console.log("inside useDebounce");
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timeoutId);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
